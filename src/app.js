818b649ec7ccec202e96918dfab8f3e025fa4a92
Sohaibkundi2 <sohaibkundi2@gmail.com>
2025-06-22 17:42:13 +0500

    Initial commit with project setup

---
+++ src/app.js
@@ -0,0 +1,24 @@
+import express from 'express'
+import dotenv from 'dotenv'
+import mongoDB from './db/index.js'
+import cors from 'cors'
+const app = express()
+dotenv.config()
+
+app.use(cors());
+app.use(express.json({ limit: '16kb' }));
+app.use(express.urlencoded({ extended: true, limit: '16kb' }));
+
+const PORT = process.env.PORT || 5000;
+
+mongoDB()
+.then(()=>{
+    app.listen(PORT, ()=>{
+    console.log(`server is listning on port ${PORT}`)
+})
+})
+.catch((err)=>{
+    console.log('err while connecting to DB', err)
+})
+
+export {app}
