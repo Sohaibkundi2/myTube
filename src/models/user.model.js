fdbacf189c4785dd320e52b0e0020dab7639f80b
Sohaibkundi2 <sohaibkundi2@gmail.com>
2025-06-25 12:09:36 +0500

    Add bcrypt and jsonwebtoken for password hashing and token generation; update user model with methods for password verification and token creation

--- src/models/user.model.js
+++ src/models/user.model.js
@@ -1,4 +1,6 @@
 import mongoose, { Schema } from 'mongoose'
+import bcrypt from 'bcrypt'
+import jwt from 'jsonwebtoken'
 
 const userSchema = new Schema(
   {
@@ -14,6 +16,7 @@ const userSchema = new Schema(
       required: true,
       unique: true,
       trim: true,
+      match: [/^\S+@\S+\.\S+$/, 'Please provide a valid email'],
     },
     fullName: {
       type: String,
@@ -43,5 +46,40 @@ const userSchema = new Schema(
   },
   { timestamps: true },
 )
+
+userSchema.pre('save',async function (next) {
+  if (!this.isModified('password')) return next()
+
+  this.password =await bcrypt.hash(this.password, 10)
+
+  next()
+})
+
+userSchema.methods.isPasswordCorrect = async function (password) {
+  return await bcrypt.compare(password, this.password)
+}
+
+userSchema.methods.generateAccessToken = function () {
+  return jwt.sign(
+    {
+      _id: this._id,
+      username: this.username,
+      email: this.email,
+    },
+    process.env.ACCESS_TOKEN_SECRET,
+    { expiresIn: process.env.ACCESS_TOKEN_EXPIRY },
+  )
+}
+
+userSchema.methods.generateRefreshToken = function () {
+  return jwt.sign(
+    {
+      _id: this._id,
+      username: this.username,
+    },
+    process.env.REFRESH_TOKEN_SECRET,
+    { expiresIn: process.env.REFRESH_TOKEN_EXPIRY },
+  )
+}
 
 export const User = mongoose.model('User', userSchema)
